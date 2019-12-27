f7df2e0efe325d850c04a46a7fc03340bb925a8c
rnitta <attinyes@gmail.com>
2019-12-27 23:05:24 +0900

    Initial Commit

---
+++ present_pretender/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  present_pretender
+//
+//  Created by rnitta on 2019/12/27.
+//  Copyright © 2019 rnitta. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
